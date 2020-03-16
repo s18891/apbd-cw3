f4ff16d01b094f9f8ab633e0ecb9edba8e0eb587
s18891 <s18891@pjwstk.edu.pl>
2020-03-16 17:02:39 +0100

    Initial commit

---
+++ WebApplication2/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        
+        // GET
+        public string GetStudents()
+        {
+            return "Jan,Adam";
+        }
+    }
+}
