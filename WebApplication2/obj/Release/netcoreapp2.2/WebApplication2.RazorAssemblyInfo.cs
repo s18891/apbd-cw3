c01763991550dae40ef4c7397c48e67b2e2b33f7
s18891 <s18891@pjwstk.edu.pl>
2020-04-27 00:27:37 +0200

    update

--- WebApplication2/obj/Release/netcoreapp2.2/WebApplication2.RazorAssemblyInfo.cs
+++ WebApplication2/obj/Release/netcoreapp2.2/WebApplication2.RazorAssemblyInfo.cs
@@ -11,13 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("WebApplication2.Views")]
+[assembly: Microsoft.AspNetCore.Razor.Hosting.RazorLanguageVersionAttribute("2.1")]
+[assembly: Microsoft.AspNetCore.Razor.Hosting.RazorConfigurationNameAttribute("MVC-2.1")]
+[assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyNameAttribute("MVC-2.1", "Microsoft.AspNetCore.Mvc.Razor.Extensions")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
 
